44ba4263e19a5788646feb59da227f6ee83cab32
Jeff Lai <jeff@oxio.io>
2022-06-21 14:20:04 -0400

    shortnumber_info: Accept pointer in APIs

--- shortnumber_info_test.go
+++ shortnumber_info_test.go
@@ -18,20 +18,20 @@ func TestIsPossibleShortNumber(t *testing.T) {
 		CountryCode:    &countryCode,
 		NationalNumber: &nationalNumber,
 	}
-	assert.True(t, IsPossibleShortNumber(*possibleNumber))
+	assert.True(t, IsPossibleShortNumber(possibleNumber))
 
 	possibleNumber, err := Parse("123456", "FR")
 	if err != nil {
 		t.Errorf("Error parsing number: %s: %s", "123456", err)
 	}
-	assert.True(t, IsPossibleShortNumberForRegion(*possibleNumber, "FR"))
+	assert.True(t, IsPossibleShortNumberForRegion(possibleNumber, "FR"))
 
 	nationalNumber = 9
 	impossibleNumber := &PhoneNumber{
 		CountryCode:    &countryCode,
 		NationalNumber: &nationalNumber,
 	}
-	assert.False(t, IsPossibleShortNumber(*impossibleNumber))
+	assert.False(t, IsPossibleShortNumber(impossibleNumber))
 
 	// Note that GB and GG share the country calling code 44, and that this number is possible but
 	// not valid.
@@ -41,7 +41,7 @@ func TestIsPossibleShortNumber(t *testing.T) {
 		CountryCode:    &countryCode,
 		NationalNumber: &nationalNumber,
 	}
-	assert.True(t, IsPossibleShortNumber(*possibleNumber))
+	assert.True(t, IsPossibleShortNumber(possibleNumber))
 }
 
 func TestIsValidShortNumber(t *testing.T) {
@@ -51,24 +51,24 @@ func TestIsValidShortNumber(t *testing.T) {
 		CountryCode:    &countryCode,
 		NationalNumber: &nationalNumber,
 	}
-	assert.True(t, IsValidShortNumber(*validNumber))
+	assert.True(t, IsValidShortNumber(validNumber))
 
 	validNumber, err := Parse("1010", "FR")
 	if err != nil {
 		t.Errorf("Error parsing number: %s: %s", "1010", err)
 	}
-	assert.True(t, IsValidShortNumberForRegion(*validNumber, "FR"))
+	assert.True(t, IsValidShortNumberForRegion(validNumber, "FR"))
 
 	nationalNumber = uint64(123456)
 	invalidNumber := &PhoneNumber{
 		CountryCode:    &countryCode,
 		NationalNumber: &nationalNumber,
 	}
-	assert.False(t, IsValidShortNumber(*invalidNumber))
+	assert.False(t, IsValidShortNumber(invalidNumber))
 
 	invalidNumber, err = Parse("123456", "FR")
 	if err != nil {
 		t.Errorf("Error parsing number: %s: %s", "1010", err)
 	}
-	assert.False(t, IsValidShortNumberForRegion(*invalidNumber, "FR"))
+	assert.False(t, IsValidShortNumberForRegion(invalidNumber, "FR"))
 }
