17ef4c050694af05d189f5170d775d251fe8c023
Norbert Kwizera <norkans7@gmail.com>
2023-05-10 12:43:37 +0200

    Add isValid display to phoneparser

--- cmd/phoneparser/main.go
+++ cmd/phoneparser/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/nyaruka/phonenumbers"
 )
@@ -12,11 +13,19 @@ func main() {
 		fmt.Println("usage: phoneparser [number] [two letter coutry]")
 		os.Exit(1)
 	}
+
 	num, err := phonenumbers.Parse(os.Args[1], os.Args[2])
 	if err != nil {
 		fmt.Printf("Error parsing number: %s\n", err)
 	}
+
+	metadata, err := phonenumbers.Parse(os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Printf("error parsing phone", err.Error())
+	}
 	fmt.Printf("            E164: %s\n", phonenumbers.Format(num, phonenumbers.E164))
 	fmt.Printf("National Dialing: %s\n", phonenumbers.Format(num, phonenumbers.NATIONAL))
 	fmt.Printf("        National: %d\n", *num.NationalNumber)
+	fmt.Printf("         IsValid: %s\n", strconv.FormatBool(phonenumbers.IsValidNumber(metadata)))
+
 }
